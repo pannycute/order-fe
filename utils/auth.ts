4496bd9d7607eee724d6c4987f63b35de4ccf33e
pannycute <irvannydiah05@gmail.com>
2025-07-15 21:38:02 +0700

    update lagi

--- utils/auth.ts
+++ utils/auth.ts
@@ -2,6 +2,7 @@
 export function checkAuthClient() {
     if (typeof window !== 'undefined') {
       const token = localStorage.getItem('token');
+      console.log(token);
       if (!token) {
         window.location.href = '/login';
       }
