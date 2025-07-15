778298a6a3f0dca111f19523bf2f9ed3653688b0
pannycute <irvannydiah05@gmail.com>
2025-07-15 21:39:56 +0700

    frist commit

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,7 +1,7 @@
 // utils/auth.ts
 export function checkAuthClient() {
     if (typeof window !== 'undefined') {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem('auth_token') || localStorage.getItem('token');
       console.log(token);
       if (!token) {
         window.location.href = '/login';
