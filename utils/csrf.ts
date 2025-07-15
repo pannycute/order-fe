ecff5f6fbd10f27aac9bcd0d24f9621fb1d74d18
pannycute <irvannydiah05@gmail.com>
2025-07-15 21:03:20 +0700

    env

--- utils/csrf.ts
+++ utils/csrf.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getCSRFToken = async () => {
     try {
-        await axios.get(process.env.NEXT_PUBLIC_API + "/sanctum/csrf-cookie")
+        await axios.get(process.env.NEXT_PUBLIC_API_URL + "/sanctum/csrf-cookie")
     } catch (error) {
         console.log(error)
     }
